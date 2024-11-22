a13942603419bdd1683f1396cb3f96e2cee6719e
rcocks <richard.cocks@gmail.com>
2024-11-22 17:54:59 +0000

    Validate message length

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using System.Net.Mime;
+using QRCoder;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton(new QRCodeGenerator());
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/qrcode", (HttpContext httpContext, string q, QRCodeGenerator qrGenerator) =>
+{
+    if (q.Length > 512)
+    {
+        return Results.BadRequest("Maximum string length is 512");
+    }
+    using QRCodeData qrCodeData = qrGenerator.CreateQrCode(q, QRCodeGenerator.ECCLevel.Q);
+    using PngByteQRCode qrCode = new(qrCodeData);
+    byte[] qrCodeImage = qrCode.GetGraphic(20, false);
+    httpContext.Response.ContentType = MediaTypeNames.Image.Png;
+    return Results.File(qrCodeImage, MediaTypeNames.Image.Png);
+});
 
 app.Run();
